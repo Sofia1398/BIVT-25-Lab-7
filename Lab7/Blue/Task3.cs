c3acbf1e4bbcda4e15936684bd188ffd33030de9
Sofia1398 <sofiarvacheva@yandex.ru>
2026-03-16 07:47:35 +0300

    Update Task3.cs

--- Lab7/Blue/Task3.cs
+++ Lab7/Blue/Task3.cs
@@ -1,6 +1,78 @@
-﻿namespace Lab7.Blue
+namespace Lab7.Blue
 {
     public class Task3
     {
+        public struct Participant
+        {
+            private string _name;
+            private string _surname;
+            private int[] _penaltyTimes; 
+
+            public string Name => _name;
+            public string Surname => _surname;
+            public int[] PenaltyTimes => (int[])_penaltyTimes.Clone();
+            public int TotalTime
+            {
+                get
+                {
+                    int sum = 0;
+                    foreach (int mark in _penaltyTimes)
+                    {
+                        sum += mark;
+                    }
+                    return sum;
+                }
+            }
+            public bool IsExpelled
+            {
+                get
+                {
+                    foreach (int mark in _penaltyTimes)
+                    {
+
+                        if (mark == 10)
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            }
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _penaltyTimes = new int[0]; 
+            }
+
+            public void PlayMatch(int time)
+            {
+                if (time == 0 || time == 2 || time == 5 || time == 10)
+                {
+                    Array.Resize(ref _penaltyTimes, _penaltyTimes.Length + 1);
+                    _penaltyTimes[_penaltyTimes.Length - 1] = time;
+                }
+            }
+
+            
+            public static void Sort(Participant[] array)
+            {
+                var sorted = array.OrderBy(p => p.TotalTime).ToArray();
+
+                Array.Copy(sorted, array, array.Length);
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Игрок: {Name} {Surname}");
+                Console.WriteLine($"Общее штрафное время: {TotalTime} минут");
+                Console.WriteLine($"Штрафы по матчам:");
+                foreach (var time in _penaltyTimes)
+                {
+                    if (time != 0) Console.Write($"{time} ");
+                }
+            }
+        }
+
     }
 }
